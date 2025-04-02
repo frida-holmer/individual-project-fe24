806b15b67857883ecdd7afb9501610e7fee8b916
frida-holmer <frida.holmer@hotmail.com>
2025-04-02 11:53:47 +0200

    Added default code.

---
+++ app/[id]/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./game.module.css";
+
+export default function GamePage() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        
+      </main>
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  );
+}
---
+++ app/category/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./category.module.css";
+
+export default function CategoryPage() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        
+      </main>
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  );
+}
