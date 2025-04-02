d04f8d8ded85d3fc11755c67caa64cf50b0d8d67
frida-holmer <frida.holmer@hotmail.com>
2025-04-02 17:07:21 +0200

    Added basic navbar styling.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import styles from "./navbar.module.css";
 
 export default function Navbar() {
     return (
-        <nav>
+        <nav className={styles.nav}>
             <Link href="/">LOGO</Link>
             <Link href="/list">My list</Link>
         </nav>
