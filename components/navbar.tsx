6e29e31095ed5cb73f07d8e1cd32ff775f472228
frida-holmer <frida.holmer@hotmail.com>
2025-04-07 15:20:27 +0200

    Added search input field in navbar component.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,29 @@
+"use client"
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./navbar.module.css";
 
 export default function Navbar() {
+    const router = useRouter();
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            router.push(`?search=${searchQuery}`);
+        }
+    }
+
     return (
         <nav className={styles.nav}>
             <Link href="/">LOGO</Link>
+            <input
+                type="text"
+                placeholder="Search game..."
+                value={searchQuery}
+                onKeyDown={handleSearch}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
             <Link href="/list">My list</Link>
         </nav>
     );
