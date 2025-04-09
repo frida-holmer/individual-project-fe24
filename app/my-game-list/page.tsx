aab9460eb42eb6fba33666a07369f749fa26b023
frida-holmer <frida.holmer@hotmail.com>
2025-04-09 12:50:01 +0200

    Added "remove-from-list" button to My Game List Page. Also updated rendering of game list. Added basic styling.

--- app/my-game-list/page.tsx
+++ app/my-game-list/page.tsx
@@ -1,26 +1,48 @@
 "use client"
 import { Game } from "@/interfaces/game";
-import GameCard from "@/components/game-card";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./mygamelist.module.css";
 
 export default function MyGameListPage() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    const myGameList = localStorage.getItem("myGameList");
-    if (myGameList) {
-      setGames(JSON.parse(myGameList));
+    const myStoredGameList = localStorage.getItem("myGameList");
+    if (myStoredGameList) {
+      setGames(JSON.parse(myStoredGameList));
     }
   }, []);
+
+  const handleRemove = (id: number) => {
+    const updatedGameList = games.filter((game) => game.id !== id);
+    setGames(updatedGameList);
+    localStorage.setItem("myGameList", JSON.stringify(updatedGameList));
+  };
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>My game list</h1>
         {games.length > 0 ? (
-          games.map((game, i) => (
-            <GameCard key={i} game={game} />
+          games.map((game) => (
+            <article key={game.id} className={styles.list}>
+              <Link href={"/game/" + game.slug}>
+                <Image
+                  src={game.background_image}
+                  alt={game.name}
+                  width={300}
+                  height={169}
+                />
+              </Link>
+              <Link href={"/game/" + game.slug}>
+                <h2>{game.name}</h2>
+              </Link>
+              <button onClick={() => handleRemove(game.id)}>
+                Remove from list
+              </button>
+            </article>
           ))
         ) : (
           <p>Your list is empty.</p>
